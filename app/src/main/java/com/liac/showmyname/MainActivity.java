2874cb8c55ecbfa78a6f03a97f0cee9817435828
liacdev <jagoblacksky@gmail.com>
2022-08-02 22:46:26 -0500

    Added Button and TextView

--- app/src/main/java/com/liac/showmyname/MainActivity.java
+++ app/src/main/java/com/liac/showmyname/MainActivity.java
@@ -3,12 +3,28 @@ package com.liac.showmyname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showButton;
+    private TextView nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showButton = findViewById(R.id.button);
+        nameText = findViewById(R.id.textView);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameText.setText("Hello from Java!");
+            }
+        });
+
     }
 }
